2b3b07faaa1d14eeb1fc821b49bbbadf9078d9fe
Davi Barbosa <davi474silva@gmail.com>
2019-12-12 16:01:07 -0300

    alteração no formulario.php

--- formulario.php
+++ formulario.php
@@ -5,11 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Formulario</title>
+    <style>
+        p{
+            font-size: 20pt;
+            color: #80a2ff;
+			text-shadow: 1px 1px 1px black;
+        }
+    </style>
 </head>
 <body>
     <?php
        $n = $_GET["tNome"];
-       echo "Olá ".$n.", muito obrigado pela mensagem. Se você viu isto quer dizer que funcionou :)";
+       echo "<p>Olá ".$n.", muito obrigado pela mensagem. Se você viu isto quer dizer que funcionou :)</p>";
     ?>
 </body>
 </html>
